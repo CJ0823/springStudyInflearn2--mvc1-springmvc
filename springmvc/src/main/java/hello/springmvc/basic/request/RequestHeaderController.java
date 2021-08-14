a89643de0dc8cd4a876de28dab391ceb35e9c3d4
CJ0823 <changjun0823@gmail.com>
2021-08-14 10:35:18 +0900

    springmvc 기본기능 완료

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -9,26 +9,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.Locale;
 
 @Slf4j
 @RestController
 public class RequestHeaderController {
 
-  @RequestMapping("/headers")
-  public String headers(HttpServletRequest request, HttpServletResponse response,
-                        HttpMethod httpMethod, Locale locale,
-                        @RequestHeader MultiValueMap<String, String> headerMap,
-                        @RequestHeader("host") String host,
-                        @CookieValue(value = "myCookie", required = false) String cookie) {
-    log.info("request={}", request);
-    log.info("response={}", response);
-    log.info("httpMethod={}", httpMethod);
-    log.info("locale={}", locale);
-    log.info("headerMap={}", headerMap);
-    log.info("header host={}", host);
-    log.info("myCookie={}", cookie);
-    return "ok";
-  }
+    @RequestMapping("/headers")
+    public String headers(HttpServletRequest request,
+                          HttpServletRequest response,
+                          HttpMethod httpMethod,
+                          Locale locale,
+                          @RequestHeader MultiValueMap<String, String> headerMap,
+                          @RequestHeader("host") String host,
+                          @CookieValue(value = "myCookie", required = false) String cookie
+                          ) {
+
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap);
+        log.info("header host={}", host);
+        log.info("myCookie={}", cookie);
+        return "ok";
+    }
 }
